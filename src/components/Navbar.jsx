2836f360278d9b2e2dc88fc796c2dd63b4a67a23
Hoanle396 <89791546+Hoanle396@users.noreply.github.com>
2022-05-18 00:18:11 +0700

    i

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,7 +8,7 @@ export default function Navbar() {
    }
    return (
       <nav className="navbar navbar-light bg-light">
-         <a className="navbar-brand" href="">
+         <a className="navbar-brand" onClick={()=>nav('/')}>
             Admin Dashboard
          </a>
          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -17,7 +17,7 @@ export default function Navbar() {
          <div className="collapse navbar-lg navbar-collapse" id="navbarNav">
             <ul className="navbar-nav">
                <li className="nav-item active">
-                  <a className="nav-link" href="">Home </a>
+                  <a className="nav-link" onClick={()=>nav('/')}>Home </a>
                </li>
                <li className="nav-item">
                   <a className="nav-link" onClick={()=>nav('user')}>User</a>
